6bc7602376869f3f0ac375996258630eb7b40b44
Juliette Pouchol <juliette@dotalign.com>
2017-06-19 16:28:24 -0400

    translated to french

--- HelloWorld/helloworld.cs
+++ HelloWorld/helloworld.cs
@@ -6,7 +6,7 @@ namespace Hello
     {
         public static void Main()
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("bonjour World!");
         }
     }
 }
